05c909bdc920c32c294ec873426ba131e3fb0181
Rohit <iamrohitlakra@gmail.com>
2019-06-13 08:51:09 +0530

    init

---
+++ app/src/main/java/com/example/myapplication/model/MenuType.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.model;
+
+public enum MenuType {
+
+    NORMAL(1001), HEADER(1002), SUB_HEADER(1003), DIVIDER(1004);
+
+    private final int value;
+
+    MenuType(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
